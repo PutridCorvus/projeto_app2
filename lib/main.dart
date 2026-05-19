45b64d0267ac5baa1a624ba73746ebcf6b82fa90
PutridCorvus <nokrondaath@gmail.com>
2026-05-19 10:26:34 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:aula15/pages/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(
+  State<MainApp> createState() => _MainAppState();
+}
 
-    )
-    
+class _MainAppState extends State<MainApp> {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(
+        
+      )
     );
   }
 }
+
+/////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////
+
